e4983ae97c50dd55914efd3d736ff4f4ba60747c
Afriza <140886763+afrzchrs@users.noreply.github.com>
2025-06-16 21:57:36 +0700

    Update playablechar.h

--- src/playablechar.h
+++ src/playablechar.h
@@ -1,14 +1,20 @@
 #ifndef playablechar_h
 #define playablechar_h
-
 #include "common.h"
 #include "inventory.h"
 #include "welcomesc.h"
 #include "skilltree.h"
 #include "skilltree_utils.h"
 #include "skilltree_save.h"
-
 #define DEFAULT 0
+
+#define MAX_SKILL 6
+
+typedef struct SkillList {
+    char skillName[50];
+    int power;
+    float scale;
+} SkillList;
 
 typedef struct tChar{
     char nama[50];
@@ -20,34 +26,30 @@ typedef struct tChar{
     int Lvl;
     int isDefending;
     addressItem inventory;
-    SkillTree* skiltree;       
-    CharType charType; 
+    SkillTree* skilltree;       
+    CharType charType;
+	SkillList* skills[MAX_SKILL];
+    int skillCount;  
 } tChar;
 
+// ciptakan karakter
 void inputCharUser(addressChar* k);
 void pilihKarakter(addressChar* k, int pilihan);
 void karakterA(addressChar* k);
 void karakterM(addressChar* k);
 void karakterD(addressChar* k);
-void Isi_Stat(addressChar* k, int *Hp, int *Att, int *Def, int *Lvl, CharType charType, const char* nama);
+void Isi_Stat(addressChar* k, int* Hp, int* Att, int* Def, int* Lvl, CharType charType, const char* nama);
 
-// INVENTORO
+// skill
+int expLevel(addressChar k);
+void levelUp(addressChar k);
+void efekSkill(addressChar k);
+void simpanSkill(addressChar k, SkillTree *node);
+
+// Inventory
 void tambahItemKeKarakter(addressChar* karakter, addressItem* I);
 void tampilkanInventoryKarakter(addressChar karakter);
 void pilihItemKarakter(addressChar karakter);
 
-// MANAJEMEN SKIL
-void applySkillEffects(addressChar character);
-void applyNodeEffects(addressChar character, SkillTree* node);
-void applyBasicEffect(addressChar character, BasicAddition* effect);
-void applyPercentageEffect(addressChar character, PercentageAddition* effect);
-int unlockSkillForCharacter(addressChar character, int nodeId);
-void showAvailableSkills(addressChar character);
-void printNodeInfo(SkillTree* node);
-
-// INFO KARAKTER
-void showCharacterStats(addressChar character);
-void destroyCharacter(addressChar character);
-addressChar createCharacterWithSkillTree(const char* name, CharType type);
 
 #endif
