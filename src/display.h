a857594598ee4a3b66c461c1ca7becd26bc27603
Afriza <140886763+afrzchrs@users.noreply.github.com>
2025-06-19 17:51:37 +0700

    Update display.h

--- src/display.h
+++ src/display.h
@@ -14,12 +14,13 @@ void pilihMenu(int pilihan);
 void welcomeScreen();
 void tampilkanASCII();
 void inputNama();
+void displayHowToPlay();
 
 void drawBorder();
 void drawMap(const Map *map);
 void drawUI(const Map *map, addressChar karakter);
 void drawBox(int startX, int startY, int width, int height);
-int drawCombatUi(addressChar *k, Enemy *enemy);
+int drawCombatUi(addressChar k, Enemy *enemy);
 void drawShopUI(addressChar *k, addressShopItem shop);
 
 #endif
